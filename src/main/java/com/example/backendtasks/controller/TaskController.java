239ebe67a2e4d9cba56fafbf5975b06ad670a40f
Fares El Heni <felhani@diginov.tech>
2023-10-21 12:04:34 +0100

    added controller

--- src/main/java/com/example/backendtasks/controller/TaskController.java
+++ src/main/java/com/example/backendtasks/controller/TaskController.java
@@ -1,4 +1,45 @@
 package com.example.backendtasks.controller;
 
+import com.example.backendtasks.model.Task;
+import com.example.backendtasks.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tasks")
 public class TaskController {
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @GetMapping
+    public List<Task> getAllTasks(){
+        return taskRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Task getTaskById(@PathVariable Long id) {
+        return taskRepository.findById(id).orElse(null);
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return taskRepository.save(task);
+    }
+
+    @PutMapping("/{id}")
+    public Task updateTask(@PathVariable Long id, @RequestBody Task updatedTask) {
+        if (taskRepository.existsById(id)) {
+            updatedTask.setId(id);
+            return taskRepository.save(updatedTask);
+        }
+        return null; // Handle not found case
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable Long id) {
+        taskRepository.deleteById(id);
+    }
+
 }
